c1b5f29a1d580a32fd235f89da02d9577aea3232
timbo9 <tntherussian@gmail.com>
2025-07-31 12:20:19 -0700

    pushing a possible fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import Sidebar from '@/components/Sidebar';
+import Sidebar from '../components/Sidebar';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
