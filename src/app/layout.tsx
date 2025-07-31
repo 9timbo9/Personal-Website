740dcfa45485f2b763c5f2677746a5f021187657
timbo9 <tntherussian@gmail.com>
2025-07-31 13:05:30 -0700

    this will work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import './globals.css';
-import Sidebar from 'src/components/Sidebar';
+import Sidebar from 'src/components/SideBar';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
